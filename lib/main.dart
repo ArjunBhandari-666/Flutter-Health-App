16097755ac89ea46a8528db62c5c63245a75980d
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2021-11-22 19:52:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,47 @@
-import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 
-import 'package:flutter_custom_widget_demo/widget/custom_button.dart';
+void main() {
+  runApp(Arjun());
+}
 
-class DemoApp extends StatelessWidget {
-  const DemoApp({Key? key}) : super(key: key);
+class Arjun extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text('Custom Widget in Flutter'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomButton(
-            onTap: () {},
-            buttonText: 'First Button',
-            buttonColor: Colors.blue,
-            textColor: Colors.white,
-            borderRadius: 10,
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Demo Arjun'),
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: CustomButton(
-                  onTap: () {},
-                  buttonText: 'second Button',
-                  textColor: Colors.black,
-                  borderRadius: 0,
-                  borderColor: Colors.red,
+          body: Center(
+              child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(25),
+              child: FlatButton(
+                child: Text(
+                  'SignUp',
+                  style: TextStyle(fontSize: 20.0),
                 ),
+                onPressed: () {},
               ),
-              Expanded(
-                child: CustomButton(
-                  onTap: () {},
-                  buttonText: 'New Button',
-                  textColor: Colors.white,
-                  borderRadius: 10,
-                  buttonColor: Colors.black,
-                  borderColor: Colors.white,
+            ),
+            Container(
+              margin: EdgeInsets.all(25),
+              child: FlatButton(
+                child: Text(
+                  'LogIn',
+                  style: TextStyle(fontSize: 20.0),
                 ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          CustomButton(
-            onTap: () {},
-            buttonText: 'Last Button',
-            textColor: Colors.green,
-            buttonColor: Colors.greenAccent.shade100,
-            borderRadius: 30,
-          )
-        ],
-      ),
+                color: Colors.blueAccent,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+          ]))),
     );
   }
 }
