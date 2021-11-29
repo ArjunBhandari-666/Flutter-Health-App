b619bc40e31e1fa4bdf8d147ecfec9628baa9108
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2021-11-29 19:27:45 +0530

    Update FancyButton.dart

--- lib/FancyButton.dart
+++ lib/FancyButton.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
 class FancyButton extends StatelessWidget {
-  const FancyButton({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Text("HELLO");
