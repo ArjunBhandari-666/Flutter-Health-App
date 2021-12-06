72059e99ef3a38174eb814d9e5874e6c170c0459
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2021-12-06 19:43:15 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,8 +5,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); 
-  Widget @override
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return (
+      child: MaterialApp(
+        home:HomePage(),
+      )
+    ) ;
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.redAccent,
