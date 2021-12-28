a76b61bedfa03b3e0ff9098e316f4a86fade6550
gg-coding <rozannagreene@gmail.com>
2021-12-28 17:57:42 +0400

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,25 +15,47 @@ class homePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My App Bar"), // title of AppBar
+        title: Text("Remote Health"), // title of AppBar
         centerTitle: true, // align the title to the center of the AppBar
         titleSpacing: 0.0, // letter spacing for title
         backgroundColor:
-            Colors.deepPurpleAccent, // background color of the AppBar
-        shadowColor: Colors.blue, // Shadow color for the AppBar
-        elevation: 10.0, // Elevation of the AppBar
+            Colors.blue.shade200, // background color of the AppBar
+        shadowColor: Colors.blue.shade700, // Shadow color for the AppBar
+        elevation:10.0, // Elevation of the AppBar
         toolbarHeight: 60.0, // Height of the toolbar
         toolbarOpacity: 1.0, // Opacity of the toolbar
-        leading: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.home)), // Leading icon with function
-        leadingWidth: 50.0, // Width of leading icon
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text,
-        ),
-      ),
-    );
+      body: Column(children: [
+        ElevatedButton(
+          child: const Text("Results Homepage"),
+        onPressed: Navigator.push(context, MaterialPageRoute(builder: BuildContext context => ResultsHomepage(),),),);
+
+
+      ElevatedButton(
+          child: Text("TREATMENT PLAN", style: TextStyle(fontFamily: HighTowerText)),
+          onPressed: Navigator.push(context, TreatmentHomepage()),
+          )
+
+      ],)
+        
+        );
+        }
+}
+
+class ResultsHomepage extends StatelessWidget {
+  const ResultsHomepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class TreatmentHomepage extends StatelessWidget {
+  const TreatmentHomepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
