68a14260422c283cbc97f6cbf9fcb41547b5b46b
gg-coding <rozannagreene@gmail.com>
2021-12-29 19:10:22 +0400

    some minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,45 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: "Image Changer",
-    home: homePage(),
+    home: HomePage(),
   ));
 }
 
-class homePage extends StatelessWidget {
-  const homePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Remote Health"), // title of AppBar
-        centerTitle: true, // align the title to the center of the AppBar
-        titleSpacing: 0.0, // letter spacing for title
-        backgroundColor:
-            Colors.blue.shade200, // background color of the AppBar
-        shadowColor: Colors.blue.shade700, // Shadow color for the AppBar
-        elevation:10.0, // Elevation of the AppBar
-        toolbarHeight: 60.0, // Height of the toolbar
-        toolbarOpacity: 1.0, // Opacity of the toolbar
-      ),
-      body: Column(children: [
-        ElevatedButton(
-          child: const Text("Results Homepage"),
-        onPressed: Navigator.push(context, MaterialPageRoute(builder: BuildContext context => ResultsHomepage(),),),);
-
-
-      ElevatedButton(
-          child: Text("TREATMENT PLAN", style: TextStyle(fontFamily: HighTowerText)),
-          onPressed: Navigator.push(context, TreatmentHomepage()),
-          )
-
-      ],)
-        
-        );
-        }
+        appBar: AppBar(
+          title: const Text("Remote Health"), // title of AppBar
+          centerTitle: true, // align the title to the center of the AppBar
+          titleSpacing: 0.0, // letter spacing for title
+          backgroundColor:
+              Colors.blue.shade200, // background color of the AppBar
+          shadowColor: Colors.blue.shade700, // Shadow color for the AppBar
+          elevation: 10.0, // Elevation of the AppBar
+          toolbarHeight: 60.0, // Height of the toolbar
+          toolbarOpacity: 1.0, // Opacity of the toolbar
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                child: const Text("Results Homepage"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ResultsHomepage()),
+                  );
+                })
+          ],
+        ));
+  }
 }
 
 class ResultsHomepage extends StatelessWidget {
@@ -47,7 +47,13 @@ class ResultsHomepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade200,
+        title: const Text("Results Review"),
+        centerTitle: true,
+      ),
+    );
   }
 }
 
